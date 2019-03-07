3bfe62ea667b88b02c4e1103b61ec8cadb974c42
Juan Saavedra <saavedrajj@gmail.com>
2019-03-07 23:15:35 +0000

    s

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,6 +1,6 @@
-<?php $baseUrl =  "http://" .$_SERVER['SERVER_NAME'] . "/checkout/"; ?>
+<?php $baseUrl =  "https://" .$_SERVER['SERVER_NAME'] . "/checkout/"; ?>
 <?php /*include "<?php echo $baseUrl;?>includes/common.php";*/ ?>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="#">Checkout</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
