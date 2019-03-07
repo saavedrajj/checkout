71024c4d9ffadf407cc3ada45ea71f040d7c11e5
Juan Saavedra <saavedrajj@gmail.com>
2019-03-07 20:02:36 +0000

    m

--- braintree/drop-in/transaction.php
+++ braintree/drop-in/transaction.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<title>Braintree - Drop-In UI</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
